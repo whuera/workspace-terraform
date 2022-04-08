373a07dee1c8e50781507924ea48090599d74103
William Huera <whuera@gmail.com>
2022-04-07 23:57:17 -0500

    add main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+  terraform {
+    backend "azure" {}
+    }
+
+    # Configure the Azure Provider
+    provider "azurerm" {
+    # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+    version = "=2.20.0"
+    features {}
+  }
+
+    # Create a resource group
+    resource "azurerm_resource_group" "example" {
+    name     = "example-resources"
+    location = "East US"
+  }
