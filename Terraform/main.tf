f60d52891555b919935b3a57d26e2bec9829e85b
William Huera <whuera@gmail.com>
2022-04-16 16:53:45 -0500

    delete resourses

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,7 +5,7 @@
     # Configure the Azure Provider
     provider "azurerm" {
     # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-    version = "=2.20.0"
+    version = "=0.12.3"
     features {}
   }
     resource "random_integer" "ri" {
