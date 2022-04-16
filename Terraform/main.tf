c24b9ee02d67f6d61ea99a9167c56a95bfabbf51
William Huera <whuera@gmail.com>
2022-04-16 10:39:47 -0500

    update resourses

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,6 @@
   terraform {
     backend "azure" {}
-    }
+  }
 
     # Configure the Azure Provider
     provider "azurerm" {
@@ -8,9 +8,45 @@
     version = "=2.20.0"
     features {}
   }
+    resource "random_integer" "ri" {
+    min = 10000
+    max = 99999
+  }
+
+    resource "azurerm_resource_group" "rg" {
+    name     = "appShoppingCartResourceGroup-${random_integer.ri.result}"
+    location = "eastus"
+  }
+
+    resource "azurerm_app_service_plan" "appserviceplan" {
+    name                = "webapp-asp-${random_integer.ri.result}"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    sku {
+    tier = "Free"
+    size = "F1"
+  }
+  }
+
+    resource "azurerm_app_service" "webapp" {
+    name                = "webapp-${random_integer.ri.result}"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+    source_control {
+    repo_url           = "https://github.com/whuera/app-shopping-cart"
+    branch             = "master"
+    manual_integration = true
+    use_mercurial      = false
+  }
+  }
+
+    resource "resorce_shoppingcart" "rg-poc-terraform" {
+    prefix    = var.resource_group_name_prefix
+  }
 
     # Create a resource group
-    resource "azurerm_resource_group" "example" {
+    resource "azurerm_resource_group" "rg-poc-terraform" {
     name     = "example-resources"
     location = "East US"
   }
